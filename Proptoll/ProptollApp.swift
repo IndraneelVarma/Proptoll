00356f6e5082876e7dc67760d18e82d3806ea101
IndraneelVarma <manthenaindraneel@gmail.com>
2024-08-07 10:33:20 +0530

    Initial Commit

---
+++ Proptoll/ProptollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProptollApp.swift
+//  Proptoll
+//
+//  Created by Indraneel Varma on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ProptollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
