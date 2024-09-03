c0a1b62a971ae33133cd02eb40a3b2d4434a4f4d
IndraneelVarma <manthenaindraneel@gmail.com>
2024-09-03 14:17:14 +0530

    fixed bug where share button in newsView wasnt appearing in certain smaller iphones

--- Proptoll/UI/Noticeboard/NewsView.swift
+++ Proptoll/UI/Noticeboard/NewsView.swift
@@ -10,57 +10,64 @@ struct NewsView: View {
     @State private var isSharePresented = false
     
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
+        ScrollView {
+            VStack(alignment: .center, spacing: 20) {
                 Text(title.htmlToString())
                     .font(.title)
                     .multilineTextAlignment(.center)
-                Spacer()
-            }
-            
-            Text(subTitle.htmlToString())
-                .font(.title3)
-                .multilineTextAlignment(.center)
-            
-            if let imageUrl = URL(string: image ?? "https://picsum.photos/200") {
-                AsyncImage(url: imageUrl) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView()
-                            .frame(width: 300, height: 200)
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 300, height: 200)
-                    case .failure:
-                        ProgressView()
-                            .frame(height: 200)
-                    @unknown default:
-                        ProgressView()
-                            .frame(height: 200)
+                
+                Text(subTitle.htmlToString())
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                
+                if let imageUrl = URL(string: image ?? "https://picsum.photos/200") {
+                    AsyncImage(url: imageUrl) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                                .frame(width: 300, height: 200)
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300, height: 200)
+                        case .failure:
+                            Image(systemName: "photo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 300, height: 200)
+                                .foregroundColor(.gray)
+                        @unknown default:
+                            ProgressView()
+                                .frame(height: 200)
+                        }
                     }
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 200)
+                        .foregroundColor(.gray)
                 }
-            } else {
-                // Fallback if no image URL is available
-            }
-            ScrollView{
+                
                 Text(content.htmlToString())
                     .padding()
             }
-            
-            Spacer()
-            
-            Button(action: {
-                isSharePresented = true
-            }) {
-                Label("Share", systemImage: "square.and.arrow.up")
-            }
             .padding()
-            .sheet(isPresented: $isSharePresented) {
-                ActivityViewController(activityItems: [deepLink])
+        }
+        .navigationTitle("")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    isSharePresented = true
+                }) {
+                    Image(systemName: "square.and.arrow.up")
+                }
             }
+        }
+        .sheet(isPresented: $isSharePresented) {
+            ActivityViewController(activityItems: [deepLink])
         }
     }
 }
@@ -78,5 +85,7 @@ struct ActivityViewController: UIViewControllerRepresentable {
 }
 
 #Preview {
-    NewsView(deepLink: "https://example.com", title: "Test", subTitle: "subtitle ajdlajdafh", content: "akdhakfhkahfkjahfkjahfa akhfkjahfkahfkahfahf alfhlkahflkahflahfla ajkfgjyafhagfkag  jagfjhagfjagfh ajhgfhjafgkjagf ajfgjahfgjhagfjh", image: nil)
+    NavigationView {
+        NewsView(deepLink: "https://example.com", title: "Test", subTitle: "subtitle ajdlajdafh", content: "akdhakfhkahfkjahfkjahfa akhfkjahfkahfkahfahf alfhlkahflkahflahfla ajkfgjyafhagfkag  jagfjhagfjagfh ajhgfhjafgkjagf ajfgjahfgjhagfjh", image: nil)
+    }
 }
